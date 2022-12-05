f5a4745b6cd5f3161bef1aab721526f9d64cfe59
Florian Dreyer <flodreyer@web.de>
2022-12-05 17:30:42 +0100

    changed the link coloring to be random for each one live during the debrief

--- index.js
+++ index.js
@@ -24,16 +24,18 @@ function addClassToLinks(cls) {
 
 function toggleClass(cls) {
     let images = document.getElementsByTagName("img");
+
     for (let i = 0; i < images.length; i++) {
         images[i].classList.toggle(cls)
     }
-    }
+}
 
 
 function colorPrices() {
-    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     let prices = document.getElementsByClassName("price");
+    
     for (let i=0; i<prices.length; i++) {
+        let randomColor = "#" + Math.floor(Math.random()*16777216).toString(16);
         prices[i].style.color = randomColor;
     }
 }
@@ -41,8 +43,9 @@ function colorPrices() {
 function comment() {
     let commentSection = document.getElementById("commentSection");
     let commentText = document.getElementById("commentArea").value;
-    document.getElementById("commentArea").value = "";
     let comment = document.createElement("div");
+
+    document.getElementById("commentArea").value = "";
     comment.innerText = commentText;
     comment.classList.add("comment");
     commentSection.appendChild(comment);
