ce64a3775d53f92bb67818cb7e180a5afccfc80d
Florian Dreyer <flodreyer@web.de>
2022-12-05 15:28:37 +0100

    basically does what it should, looks BAD though...

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-function doStuff() {
-    let elem = document.getElementById('bruh');
-
-    return elem.innerHTML;
+function changeH1(str) {
+    let header = document.getElementsByTagName("h1");
+    header[0].innerText = str;
 }
 
+function changeBackground() {
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function changeAddress(street, city, country) {
+    let address = document.getElementById("address");
+    address.children[0].innerText = street;
+    address.children[1].innerText = city;
+    address.children[2].innerText = country;
+}
+
+function addClassToLinks(cls) {
+    let links = document.getElementsByClassName("shoplink");
+
+    for (let i=0; i<links.length; i++) {
+        links[i].classList.add(cls);
+    }
+}
+
+function toggleClass(cls) {
+    let images = document.getElementsByTagName("img");
+    for (let i = 0; i < images.length; i++) {
+        images[i].classList.toggle(cls)
+    }
+    }
+
+
+function colorPrices() {
+    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    let prices = document.getElementsByClassName("price");
+    for (let i=0; i<prices.length; i++) {
+        prices[i].style.color = randomColor;
+    }
+}
